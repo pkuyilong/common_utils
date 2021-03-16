2c0233dcdd0f630485d774b0952f7e66d00c7951
mayilong <mayilong@pku.edu.cn>
2021-03-16 11:55:26 +0800

    Update show.sh

--- show.sh
+++ show.sh
@@ -1,9 +1,21 @@
-
 PYTHON_INTERPRETER=/home/ssd1/zhaoguangwei/python_env/bin/python
 
-help(){
+function help(){
     echo "Usage:
-        show -p {port} -P {path/to/show/folder}"
+        1. show -p {port}
+            default path is current working directory
+
+        2. show -p {port} -P {path/to/show/folder}"
+}
+
+function check_occupy(){
+    port=$1
+    occupy=$(netstat -ntulp | grep ${port})
+    if [[ -n "${occupy}" ]]
+    then
+        return 1
+    fi
+    return 0
 }
 
 
@@ -31,21 +43,37 @@ echo "path is ${path}"
 
 if [[ -n "${port}" ]]
 then
-    occupy=$(netstat -ntulp | grep ${port})
-    if [[ -n "${occupy}" ]]
+    check_occupy ${port}
+    if [[ $? -ne 0 ]]
     then
         echo "Port $port is Occupied!"
         exit
     fi
 else
-    echo "Please set port"
-    exit;
+    while true
+    do
+        port=$((8000 + $RANDOM % 1000))
+        check_occupy ${port}
+        if [[ $? -eq 0 ]]
+        then
+            echo "Random port is $port"
+            break
+        else
+            port=$((8000 + $RANDOM % 1000))
+        fi
+    done
 fi
 
 if [[ -z "${path}" ]]
 then
     path=$(pwd)
+else
     cd ${path}
+    echo "Jump to $path"
 fi
 
 nohup ${PYTHON_INTERPRETER} -m ComplexHTTPServer ${port} 2>&1 & >/dev/null
+if  [[ $? == 0 ]]
+then
+    echo "Start serving"
+fi
